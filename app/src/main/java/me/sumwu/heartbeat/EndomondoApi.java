bc03871f6792b51747351b4024c9fae90039a688
jusjmkim <justinjmkim@gmail.com>
2014-10-18 16:47:56 -0400

    Set up basic endomondo calculation

--- app/src/main/java/me/sumwu/heartbeat/EndomondoApi.java
+++ app/src/main/java/me/sumwu/heartbeat/EndomondoApi.java
@@ -17,6 +17,31 @@ import org.json.JSONObject;
  */
 public class EndomondoApi {
 
+    private static final String BASE_URL = "https://api.endomondo.com/api";
+    private static AsyncHttpClient client = new AsyncHttpClient();
 
+    public static int get(android.content.Context context,
+                                 java.lang.String url,
+                                 org.apache.http.Header[] headers,
+                                 RequestParams params,
+                                 java.lang.String contentType,
+                                 ResponseHandlerInterface responseHandler) {
+        client.get(context, getAbsoluteUrl(url), headers, params, contentType, responseHandler)
+    }
 
+    //This should be abstracted into a separate class, so all the API files can access it
+    private static String getAbsoluteUrl(String relativeUrl) { return BASE_URL + relativeUrl;}
+
+    private static int parseJson(org.json.JSONObject workoutResults) {
+        int[] parsedWorkoutResults = new int[2];
+        JSONObject data = parsedWorkoutResults.get("data")[0];
+        parsedWorkoutResults[0] = data.get("steps_total");
+        parsedWorkoutResults[1] = data.get("duration_total");
+        return calculatePace(parsedWorkoutResults);
+    }
+
+    private static int calculatePace(int[] paceData) {
+        int workoutMinutes = paceData[1] / 60;
+        return paceData[0] / workoutMinutes;
+    }
 }
--- app/src/main/java/me/sumwu/heartbeat/MisfitApi.java
+++ app/src/main/java/me/sumwu/heartbeat/MisfitApi.java
@@ -16,4 +16,32 @@ import org.json.JSONObject;
  * Created by justin on 10/18/14.
  */
 public class MisfitApi {
+
+
+    private static final String BASE_URL = "https://api.misfitwearables.com/";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static int get(android.content.Context context,
+                          java.lang.String url,
+                          org.apache.http.Header[] headers,
+                          RequestParams params,
+                          java.lang.String contentType,
+                          ResponseHandlerInterface responseHandler) {
+        client.get(context, getAbsoluteUrl(url), headers, params, contentType, responseHandler)
+    }
+
+    //This should be abstracted into a separate class, so all the API files can access it
+    private static String getAbsoluteUrl(String relativeUrl) { return BASE_URL + relativeUrl;}
+
+    private static int parseJson(org.json.JSONObject workoutResults) {
+        int[] parsedWorkoutResults = new int[2];
+        parsedWorkoutResults[0] = ("steps", workoutResults.get("steps"));
+        parsedWorkoutResults[1] = ("distance", workoutResults.get("distance"));
+        return calculatePace(parsedWorkoutResults);
+    }
+
+    private static int calculatePace(int[] paceData) {
+        
+    }
+
 }
