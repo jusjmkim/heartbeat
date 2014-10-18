c54eb1adf99fe3f43acbb31bb234f6c530bbd43b
jusjmkim <justinjmkim@gmail.com>
2014-10-18 15:34:37 -0400

    Make files and boilerplate lines

---
+++ app/src/main/java/me/sumwu/heartbeat/CalculateBpm.java
@@ -0,0 +1,12 @@
+package me.sumwu.heartbeat;
+
+/**
+ * Created by justin on 10/18/14.
+ */
+public class CalculateBpm {
+
+    public static int songBpm (double pace) {
+
+    }
+
+}
---
+++ app/src/main/java/me/sumwu/heartbeat/MisfitApi.java
@@ -0,0 +1,19 @@
+package me.sumwu.heartbeat;
+
+import android.content.Context;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.ResponseHandlerInterface;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * Created by justin on 10/18/14.
+ */
+public class MisfitApi {
+}
