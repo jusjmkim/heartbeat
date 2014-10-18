98eb31f93eb803157826739a37111813326f9510
jusjmkim <justinjmkim@gmail.com>
2014-10-18 17:20:16 -0400

    separate accessing api and parsing it into different files

--- app/src/main/java/me/sumwu/heartbeat/EndomondoApi.java
+++ app/src/main/java/me/sumwu/heartbeat/EndomondoApi.java
@@ -19,23 +19,11 @@ public class EndomondoApi {
     public static int get(java.lang.String url,
                           RequestParams params,
                           ResponseHandlerInterface responseHandler) {
-        client.get(getAbsoluteUrl("/1/workouts"), params, responseHandler);
+        // url should be "/1/workouts"
+        client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
     private static String getAbsoluteUrl(String relativeUrl) { return BASE_URL + relativeUrl;}
-
-    private static int parseJson(org.json.JSONObject workoutResults) {
-        int[] parsedWorkoutResults = new int[2];
-        JSONObject data = parsedWorkoutResults.get("data")[0];
-        parsedWorkoutResults[0] = data.get("steps_total");
-        parsedWorkoutResults[1] = data.get("duration_total");
-        return calculatePace(parsedWorkoutResults);
-    }
-
-    private static int calculatePace(int[] paceData) {
-        int workoutMinutes = paceData[1] / 60;
-        return (int) paceData[0] / workoutMinutes;
-    }
 
 }
 
--- app/src/main/java/me/sumwu/heartbeat/MisfitApi.java
+++ app/src/main/java/me/sumwu/heartbeat/MisfitApi.java
@@ -19,23 +19,11 @@ public class MisfitApi {
     public static int get(java.lang.String url,
                           RequestParams params,
                           ResponseHandlerInterface responseHandler) {
-        client.get(getAbsoluteUrl("/move/resource/v1/user/me/activity/sessions"), params, responseHandler);
+        // url should be "/move/resource/v1/user/me/activity/sessions"
+        client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
     private static String getAbsoluteUrl(String relativeUrl) { return BASE_URL + relativeUrl;}
-
-    private static int parseJson(org.json.JSONObject workoutResults) {
-        int[] parsedWorkoutResults = new int[2];
-        JSONObject data = parsedWorkoutResults.get("sessions")[0];
-        parsedWorkoutResults[0] = data.get("steps");
-        parsedWorkoutResults[1] = data.get("duration");
-        return calculatePace(parsedWorkoutResults);
-    }
-
-    private static int calculatePace(int[] paceData) {
-        int workoutMinutes = paceData[1] / 60;
-        return (int) paceData[0] / workoutMinutes;
-    }
 
 }
 
